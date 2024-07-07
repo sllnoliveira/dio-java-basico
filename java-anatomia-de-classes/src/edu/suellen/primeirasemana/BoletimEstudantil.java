367d423c1982453d9271013872d52fb3e731557f
Suellen Oliveira <suellen.oliveira94@outlook.com>
2024-07-07 13:17:00 -0400

    atualizando: criação de pastas

---
+++ java-anatomia-de-classes/src/edu/suellen/primeirasemana/BoletimEstudantil.java
@@ -0,0 +1,11 @@
+public class BoletimEstudantil {
+public static void main(String[] args) {
+    int mediaFinal = 8;
+    if (mediaFinal < 6)
+        System.out.println("REPROVADO");
+    else if (mediaFinal == 6)
+        System.out.println("PROVA MINERVA");
+    else
+        System.err.println("APROVADO");   
+    }
+}
---
+++ java-anatomia-de-classes/src/edu/suellen/segundasemana/MinhaClasse.java
@@ -0,0 +1,7 @@
+package edu.suellen.segundasemana;
+public class MinhaClasse {
+    
+public static void main (String [] args) {b
+}
+}
+
