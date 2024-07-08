5424f3b7e8795b56698a85e28a85598acdb42614
Suellen Oliveira <suellen.oliveira94@outlook.com>
2024-07-08 08:22:09 -0400

    attnew

--- TIPOS-VARIAVEISS/tiposvariaveiss/src/tiposvariaveiss.java
+++ TIPOS-VARIAVEISS/tiposvariaveiss/src/tiposvariaveiss.java
@@ -1,5 +1,24 @@
-public class App {
+public class tiposvariaveiss {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //estudem a classe String que representa textos
+
+        String meuNome = "Paulo Gustavo";
+
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10; 
+
+        System.out.println(numero);
+
+        final double VALOR DE PI = 3.14;
+
     }
 }
