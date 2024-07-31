72e4347234aa43ae212d723e0c998b43dba8b9d2
Suellen Oliveira <suellen.oliveira94@outlook.com>
2024-07-31 13:57:17 -0400

    Operadores Rela cionais - String

--- TIPOS-VARIAVEISS/tiposvariaveiss/src/Operadores.java
+++ TIPOS-VARIAVEISS/tiposvariaveiss/src/Operadores.java
@@ -1,11 +1,23 @@
 public class Operadores {
     public static void main(String[] args){
        //operadores relacionais
-        
+       String nomeUm = "GLAYSON";
+       String nomeDois = "GLAYSON";
+
+       System.out.println(nomeUm.equals(nomeDois));
+       
+
+
+
        int numero1 = 1;
        int nuemro2 = 2;
 
      boolean simNao = numero1 == nuemro2;
+
+    if (numero1 < nuemro2) {
+        System.out.println("A nossa condição é verdadeira");
+        
+    }
 
        System.out.println("numeroUm é igual a nuemroDois? " + simNao);
 
